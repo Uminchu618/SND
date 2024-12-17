dd374b13ba838da3ff5338f5404e039b2fc04373
uminchu <uminchu@uminchu.jp>
2024-12-17 13:09:47 +0000

    modified:   init.sh

--- init.sh
+++ init.sh
@@ -2,14 +2,15 @@
 apt install -y software-properties-common
 add-apt-repository ppa:deadsnakes/ppa
 apt install -y python3.10-dev python3.10-venv
-python3.10 -m venv venv
-source ./venv/bin/activate
+python3.10 -m venv ../venv
+source ../venv/bin/activate
 
-# git config --global --add user.name uminchu
-# git config --global --add user.email uminchu@uminchu.jp
+git config --global --add user.name uminchu
+git config --global --add user.email uminchu@uminchu.jp
 
-git clone https://github.com/Iskandor/SND.git
-cd SND/
+# git clone https://github.com/Uminchu618/SND.git
+# cd SND/
+
 pip install -r requirements.txt
 pip install torch procgen psutil tqdm torchvision ray etaprogress matplotlib
 pip install gym[atari,accept-rom-license]
